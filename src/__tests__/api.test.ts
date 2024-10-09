b6e601e5b6510dc1148b903291a20f64caa0d90e
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-09 21:57:49 +0200

    wip

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from "vitest";
+import { expect, describe, it } from "vitest";
 import { FlagsConfiguration } from "../types";
 import { buildEvaluationMachine } from "../";
 
