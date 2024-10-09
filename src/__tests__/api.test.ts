76fdc15b3a5ad0ec02a9b0b3c71094d9e2883e2e
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-09 21:40:22 +0200

    wip

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -121,5 +121,199 @@ describe("api", () => {
 
       expect(machine.evaluateAll()).toEqual({ "new-homepage": false });
     });
+
+    it("returns 'Control' variant when the flag is enabled and with __id being 'A'", () => {
+      const flagsConfig: FlagsConfiguration = [
+        {
+          key: "new-homepage",
+          status: "enabled",
+          strategies: [
+            {
+              name: "default",
+              rules: [],
+              variants: [
+                {
+                  name: "Control",
+                  percent: 90,
+                },
+                {
+                  name: "B",
+                  percent: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      const machine = buildEvaluationMachine(flagsConfig, {
+        __id: "A",
+        notMatching: "hello",
+      });
+
+      expect(machine.evaluateAll()).toEqual({ "new-homepage": "Control" });
+    });
+
+    it("returns 'B' variant when the flag is enabled and with __id being 'yo'", () => {
+      const flagsConfig: FlagsConfiguration = [
+        {
+          key: "new-homepage",
+          status: "enabled",
+          strategies: [
+            {
+              name: "default",
+              rules: [],
+              variants: [
+                {
+                  name: "Control",
+                  percent: 90,
+                },
+                {
+                  name: "B",
+                  percent: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      const machine = buildEvaluationMachine(flagsConfig, {
+        __id: "yo",
+        notMatching: "hello",
+      });
+
+      expect(machine.evaluateAll()).toEqual({ "new-homepage": "B" });
+    });
+
+    it("resolves the variants 'Control' of the matching strategy for the __id 'A'", () => {
+      const flagsConfig: FlagsConfiguration = [
+        {
+          key: "new-homepage",
+          status: "enabled",
+          strategies: [
+            {
+              name: "other",
+              rules: [
+                {
+                  field: "country",
+                  operator: "equals",
+                  value: "US",
+                },
+              ],
+              variants: [
+                {
+                  name: "OtherControl",
+                  percent: 90,
+                },
+                {
+                  name: "OtherB",
+                  percent: 10,
+                },
+              ],
+            },
+            {
+              name: "default",
+              rules: [
+                {
+                  field: "firstName",
+                  operator: "contains",
+                  value: "marvin",
+                },
+                {
+                  field: "lastName",
+                  operator: "contains",
+                  value: "frachet",
+                },
+              ],
+              variants: [
+                {
+                  name: "Control",
+                  percent: 90,
+                },
+                {
+                  name: "B",
+                  percent: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      const machine = buildEvaluationMachine(flagsConfig, {
+        __id: "A",
+        firstName: "marvin",
+        lastName: "frachet",
+        country: "FR",
+      });
+
+      expect(machine.evaluateAll()).toEqual({ "new-homepage": "Control" });
+    });
+
+    it("resolves the variants 'B' of the matching strategy for the __id 'yo'", () => {
+      const flagsConfig: FlagsConfiguration = [
+        {
+          key: "new-homepage",
+          status: "enabled",
+          strategies: [
+            {
+              name: "other",
+              rules: [
+                {
+                  field: "country",
+                  operator: "equals",
+                  value: "US",
+                },
+              ],
+              variants: [
+                {
+                  name: "OtherControl",
+                  percent: 90,
+                },
+                {
+                  name: "OtherB",
+                  percent: 10,
+                },
+              ],
+            },
+            {
+              name: "default",
+              rules: [
+                {
+                  field: "firstName",
+                  operator: "contains",
+                  value: "marvin",
+                },
+                {
+                  field: "lastName",
+                  operator: "contains",
+                  value: "frachet",
+                },
+              ],
+              variants: [
+                {
+                  name: "Control",
+                  percent: 90,
+                },
+                {
+                  name: "B",
+                  percent: 10,
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      const machine = buildEvaluationMachine(flagsConfig, {
+        __id: "yo",
+        firstName: "marvin",
+        lastName: "frachet",
+        country: "FR",
+      });
+
+      expect(machine.evaluateAll()).toEqual({ "new-homepage": "B" });
+    });
   });
 });
