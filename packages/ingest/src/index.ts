6ccd6c51e156f36951fdf83b72aff626567473f8
mfrachet <marvin@mastra.ai>
2025-06-27 18:31:52 +0200

    wip

--- packages/ingest/src/index.ts
+++ packages/ingest/src/index.ts
@@ -1,5 +1,8 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
+ * Welcome to Cloudflare Workers!
+ *
+ * This is a template for a Queue consumer: a Worker that can consume from a
+ * Queue: https://developers.cloudflare.com/queues/get-started/
  *
  * - Run `npm run dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
@@ -12,7 +15,27 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	// Our fetch handler is invoked on a HTTP request: we can send a message to a queue
+	// during (or after) a request.
+	// https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
+	async fetch(req, env, ctx): Promise<Response> {
+		// To send a message on a queue, we need to create the queue first
+		// https://developers.cloudflare.com/queues/get-started/#3-create-a-queue
+		await env['ANALYTICS-QUEUE'].send({
+			url: req.url,
+			method: req.method,
+			headers: Object.fromEntries(req.headers),
+		});
+		return new Response('Sent message to the queue');
 	},
-} satisfies ExportedHandler<Env>;
+	// The queue handler is invoked when a batch of messages is ready to be delivered
+	// https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
+	async queue(batch, env): Promise<void> {
+		// A queue consumer can make requests to other endpoints on the Internet,
+		// write to R2 object storage, query a D1 Database, and much more.
+		for (let message of batch.messages) {
+			// Process each message (we'll just log these)
+			console.log(`message ${message.id} processed: ${JSON.stringify(message.body)}`);
+		}
+	},
+} satisfies ExportedHandler<Env, Error>;
